0c96714f4011d95b5d1536efe0d8b544ef88a592
ranz3r <maksat.buribaev@gmail.com>
2025-01-19 02:33:29 +0500

    additional commit

---
+++ strings.py
@@ -0,0 +1,14 @@
+print("It's alright")
+print("He is called 'Johnny'")
+print('He is called "Johnny"')
+a = "Hello"
+print(a)
+
+b = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+print(b)
+
+g = "Hello, World!"
+print(g[1])
