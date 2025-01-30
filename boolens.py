8e605e2576a274bb91f3492c8ff7bfc338697b1c
ranz3r <maksat.buribaev@gmail.com>
2025-01-30 15:22:23 +0500

    Add files via upload

---
+++ boolens.py
@@ -0,0 +1,28 @@
+print(10 > 9)
+print(10 == 9)
+print(10 < 9)
+
+
+
+a = 200
+b = 33
+
+if b > a:
+  print("b is greater than a")
+else:
+  print("b is not greater than a")
+
+
+
+
+x = "Hello"
+y = 15
+
+print(bool(x))
+print(bool(y))
+
+
+
+bool("abc")
+bool(123)
+bool(["apple", "cherry", "banana"])
