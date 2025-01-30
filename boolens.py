e3752e1592a1e01150c6652234a464ec9b1647a0
ranz3r <maksat.buribaev@gmail.com>
2025-01-30 15:23:26 +0500

    Update boolens.py

--- boolens.py
+++ boolens.py
@@ -26,3 +26,44 @@ print(bool(y))
 bool("abc")
 bool(123)
 bool(["apple", "cherry", "banana"])
+
+
+bool(False)
+bool(None)
+bool(0)
+bool("")
+bool(())
+bool([])
+bool({})
+
+
+
+class myclass():
+  def __len__(self):
+    return 0
+
+myobj = myclass()
+print(bool(myobj))
+
+
+
+def myFunction() :
+  return True
+
+print(myFunction())
+
+
+
+
+def myFunction() :
+  return True
+
+if myFunction():
+  print("YES!")
+else:
+  print("NO!")
+
+
+
+  x = 200
+print(isinstance(x, int))
