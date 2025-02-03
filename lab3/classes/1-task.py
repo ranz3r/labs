b58f834890c7eb6aedeec24ca28d0a4d5972098f
ranz3r <maksat.buribaev@gmail.com>
2025-02-03 16:54:46 +0500

    3  lab commit

---
+++ lab3/classes/1-task.py
@@ -0,0 +1,13 @@
+class String:
+    def __init__(self):
+        self.input_string = ""
+
+    def getString(self):
+        self.input_string = input("Enter a string: ")
+
+    def printString(self):
+        print(self.input_string.upper())
+
+my_string = String()
+my_string.getString()
+my_string.printString()
